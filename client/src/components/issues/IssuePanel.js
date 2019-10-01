56acc9fdc61b6ea738babeafdceb790f73a4931a
Alexander Varney <alex@varney.me>
2019-10-01 01:02:13 -0400

    Use ChartJS doughnut chart

--- client/src/components/issues/IssuePanel.js
+++ client/src/components/issues/IssuePanel.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import styles from './css/IssuePanel.module.css';
-import ReactMarkdown from 'react-markdown';
-import CandidatePositionButton from './CandidatePositionButton';
-import PieChart from 'react-minimal-pie-chart';
+import React from 'react'
+import { connect } from 'react-redux'
+import styles from './css/IssuePanel.module.css'
+import ReactMarkdown from 'react-markdown'
+import CandidatePositionButton from './CandidatePositionButton'
+import PositionSupportChart from './PositionSupportChart'
 
 const IssuePanel = props => {
   const { issue, contest, candidates } = props
@@ -46,52 +46,40 @@ const IssuePanel = props => {
       a.candidate.status > b.candidate.status ? 1 : -1
     )
     : []
+  const unknown = positions.unknown
+    ? positions.unknown.sort((a, b) =>
+      a.candidate.status > b.candidate.status ? 1 : -1
+    )
+    : []
 
-  const totalPositions = supports.length + mixed.length + opposed.length
+  const totalPositions = supports.length + mixed.length + opposed.length + unknown.length
 
-  const chartData = [
-    {
-      title: `Supports - ${(supports.length * 100) / totalPositions}%`,
-      value: supports.length,
-      color: '#0d9900'
-    },
-    {
-      title: `Mixed - ${(mixed.length * 100) / totalPositions}%`,
-      value: mixed.length,
-      color: '#c2bb00'
-    },
-    {
-      title: `Opposed - ${(opposed.length * 100) / totalPositions}%`,
-      value: opposed.length,
-      color: '#ac2715'
-    }
-  ]
   return (
     <div className={styles.container}>
       <h1>{props.issue.name}</h1>
 
       <hr />
 
+      <div className={styles.pieChart}>
+        <PositionSupportChart positions={positions} />
+      </div>
+
+      <div className={styles.pieChartLabels}>
+        <p className={styles.pieLabelSupports}>
+            Supports - {Math.floor((supports.length * 100) / totalPositions)}%
+        </p>
+        <p className={styles.pieLabelMixed}>
+            Mixed - {Math.floor((mixed.length * 100) / totalPositions)}%
+        </p>
+        <p className={styles.pieLabelOpposed}>
+            Opposed - {Math.floor((opposed.length * 100) / totalPositions)}%
+        </p>
+        <p className={styles.pieLabelUnknown}>
+            Unknown - {Math.floor((unknown.length * 100) / totalPositions)}%
+        </p>
+      </div>
+
       <div className={styles.positionContainer}>
-        <div className={styles.pieContainer}>
-          <p>Candidate Positions</p>
-          <br />
-
-          <PieChart className={styles.pieChart} data={chartData} />
-
-          <div className={styles.pieChartLabels}>
-            <p className={styles.pieLabelSupports}>
-              Supports - {Math.floor((supports.length * 100) / totalPositions)}%
-            </p>
-            <p className={styles.pieLabelMixed}>
-              Mixed - {Math.floor((mixed.length * 100) / totalPositions)}%
-            </p>
-            <p className={styles.pieLabelOpposed}>
-              Opposed - {Math.floor((opposed.length * 100) / totalPositions)}%
-            </p>
-          </div>
-        </div>
-
         <div>
           <h4>
             <i className='far fa-check-circle' /> Supports - (
@@ -109,8 +97,7 @@ const IssuePanel = props => {
 
         <div>
           <h4>
-            <i className='fas fa-adjust' /> Mixed - (
-            {mixed ? mixed.length : 0})
+            <i className='fas fa-adjust' /> Mixed - ({mixed ? mixed.length : 0})
           </h4>
           {mixed
             ? mixed.map(item => (
@@ -136,12 +123,28 @@ const IssuePanel = props => {
             ))
             : null}
         </div>
+        {unknown && unknown.length > 0 ? (
+          <div>
+            <h4>
+            <i className="far fa-circle"></i> Unknown - (
+              {unknown ? unknown.length : 0})
+            </h4>
+            {unknown
+              ? unknown.map(item => (
+                <CandidatePositionButton
+                  key={item.candidate._id}
+                  candidate={item.candidate}
+                />
+              ))
+              : null}
+          </div>
+        ) : null}
       </div>
       <hr />
       <ReactMarkdown source={issue.description} />
     </div>
   )
-};
+}
 
 const mapStateToProps = state => ({
   candidates: state.candidates.candidates
---
+++ client/src/components/issues/PositionSupportChart.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Doughnut } from 'react-chartjs-2'
+
+export default function PositionSupportChart (props) {
+  const supports = props.positions.supports ? props.positions.supports.length : 0
+  const mixed = props.positions.mixed ? props.positions.mixed.length : 0
+  const opposed = props.positions.opposed ? props.positions.opposed.length : 0
+  const unknown = props.positions.unknown ? props.positions.unknown.length : 0
+  
+  const totalPositions = supports + mixed + opposed + unknown
+
+  const supportsPercentage = Math.floor((supports * 100) / totalPositions)
+  const mixedPercentage = Math.floor((mixed * 100) / totalPositions)
+  const opposedPercentage = Math.floor((opposed * 100) / totalPositions)
+  const unknownPercentage = Math.floor((unknown * 100) / totalPositions)
+
+  
+  const chartData = {
+    labels: ['Supports', 'Mixed', 'Opposed', 'Unknown'],
+    datasets: [
+      {
+        data: [supports, mixed, opposed, unknown],
+        backgroundColor: ['#B1DE9C', '#F5DF9F', '#EBA598', '#B9B5C7']
+      }
+    ]
+  }
+
+  return (
+    <Doughnut data={chartData} />
+  )
+}
